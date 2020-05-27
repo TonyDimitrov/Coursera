20c1d966a8714a123df93f9a3686507dfa4bb9a9
Toni <01tonydimitrov@gmail.com>
2020-05-27 12:36:11 +0300

    Initial project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coursera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
